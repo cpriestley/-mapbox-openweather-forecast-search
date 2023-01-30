5e0c4ebcd33615f61b7f6428896f874fc26239b6
Clayton Priestley <clayton.priestley@gmail.com>
2023-01-29 20:55:32 -0600

    Refactor window scroll function - added boolean logic to determine if user is scrolling up or down - added boolean logic to determine if clouds are visible or not - revised to only execute toggle when threshold is crossed and clouds are in the incorrect state (visible or hidden) - refactored individual addClass and removeClass methods to use toggleClass and extracted this logic to a single method

--- js/main-mobile.js
+++ js/main-mobile.js
@@ -14,12 +14,10 @@ $(function () {
     let main = document.querySelector("main");
 
     $("#btn-modal").click(function () {
-        map.height("100%");
-        map.width("100%");
+        map.attr("style", "height: 100%; width: 100%;");
         map.appendTo(".modal-body");
         map.removeClass("rounded-5");
         mapbox.resize();
-        //window.dispatchEvent(new Event('resize'));
     });
 
     $(".btn-close").click(function () {
@@ -30,29 +28,31 @@ $(function () {
     });
 
     $(window).scroll(scrollFunction);
+    let TOP_PADDING = 256;
 
     function scrollFunction() {
-        let top = main.getBoundingClientRect().top + 256;
-        if (top < 200 && top < lastTop) {
-            btn.removeClass("display-6")
-                .addClass("h3");
-            div.removeClass("display-1")
-                .addClass("display-6");
-            header.height("3em")
-                .css("width", "100%")
-                .css("background-color", "black");
-            clouds.height("3em")
-                .hide();
-        } else {
-            div.removeClass("display-6")
-                .addClass("display-1");
-            btn.removeClass("h3")
-                .addClass("display-6");
-            clouds.show()
-                .height("16em");
-            header.height("16em")
-                .css("background-color", "transparent");
+        let top = main.getBoundingClientRect().top + TOP_PADDING;
+        let isAscendingAndAboveThreshold = top < 200 && top < lastTop;
+        let isDescendingAndBelowThreshold = top > 50 && top > lastTop;
+        let cloudsVisible = clouds.is(":visible");
+
+        if (isAscendingAndAboveThreshold && cloudsVisible) {
+            toggleClasses(isAscendingAndAboveThreshold);
+        } else if (isDescendingAndBelowThreshold && !cloudsVisible) {
+            toggleClasses(isAscendingAndAboveThreshold);
         }
+
         lastTop = top;
+    }
+
+    function toggleClasses(isAscending) {
+        btn.toggleClass("display-6", !isAscending)
+            .toggleClass("h3", isAscending);
+        div.toggleClass("display-1", !isAscending)
+            .toggleClass("display-6", isAscending);
+        header.height(isAscending ? "3em" : "16em")
+            .css("background-color", isAscending ? "black" : "transparent");
+        clouds.toggle()
+            .height(isAscending ? "3em" : "16em")
     }
 });
